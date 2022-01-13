ede59fac9e602b45407806189d17fe78354a4320
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2022-01-14 08:27:11 +0900

    add campaign point

--- src/App.js
+++ src/App.js
@@ -13,9 +13,15 @@ function App() {
     left: 50,
     right: 100,
   };
-  const contentWidth = 400;
+  const contentWidth = 600;
   const contentHeight = 700;
-  
+
+  const difference = (date1, date2) => {
+    const date1utc = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+    const date2utc = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+    const day = 1000*60*60*24;
+    return(date2utc - date1utc)/day;
+  }
   
   useEffect(() => {
     (async () => {
@@ -55,10 +61,35 @@ function App() {
   });
 
   console.log(color)
+
+  const tu = ["20210802", "20211213", "20211214", "20211222", "20211226", "20210128", "20210208","20210222","20211011","20211011","20211017", "20211231", "20210501"];
+  console.log(tu);
+  const time = tu.map((item) => {
+    return new Date(+item.substring(0, 4), +item.substring(4, 6)-1, +item.substring(6, 8));
+  });
+
+  console.log(time);
+  time.sort((a, b) => {
+    return (a > b ? 1 : -1);
+  });
+
+  console.log(time);
+
+  const timeDomain = [0];
+
+  for(let i = 4; i < time.length; i++) {
+    timeDomain.push(difference(time[3], time[i]));
+  }
+
+  console.log("####");
+  console.log(timeDomain);
+
+
   //const color = d3.scaleLinear().range(['white', 'red']).domain([Math.min(...heatData), Math.max(...heatData)])
   const svgWidth = margin.left + margin.right + contentWidth;
   const svgHeight = margin.top + margin.bottom + contentHeight;
   const scale = d3.scaleBand().range([0, contentWidth/ 1.5]).domain(d3.range(heatData.length));
+  const campaignScale = d3.scaleLinear().domain([Math.min(...timeDomain), Math.max(...timeDomain)]).range([150, 550]);
   //let idx = 0;
     return (
       <div>
@@ -82,12 +113,12 @@ function App() {
       >
 
         <g>
-          <text x = {156} y = {0} fontSize={10}>5月</text>
-          <text x = {185} y = {0} fontSize={10}>6月</text>
-          <text x = {210} y = {0} fontSize={10}>7月</text>
-          <text x = {235} y = {0} fontSize={10}>8月</text>
-          <text x = {260} y = {0} fontSize={10}>9月</text>
-          <text x = {285} y = {0} fontSize={10}>10月</text>
+          <text x = {156} y = {0} fontSize={8}>5月</text>
+          <text x = {185} y = {0} fontSize={8}>6月</text>
+          <text x = {210} y = {0} fontSize={8}>7月</text>
+          <text x = {235} y = {0} fontSize={8}>8月</text>
+          <text x = {260} y = {0} fontSize={8}>9月</text>
+          <text x = {285} y = {0} fontSize={8}>10月</text>
         </g>
           <g>
             {heatData.map((array, i)=> {
@@ -107,10 +138,10 @@ function App() {
                     >{data[i]['title']}</text>
 
                 <rect 
-                  x = {scale(j) + 150}
-                  y = {50*i + 10}
-                  width={scale.bandwidth()}
-                  height={scale.bandwidth()}
+                  x = {50*j + 150}
+                  y = {40*i + 10}
+                  width={50}
+                  height={31}
                   fill={color[i](item)}
                 />
                 </g>
@@ -124,7 +155,20 @@ function App() {
           }
           </g>
 
-    
+          <g>
+            {timeDomain.map((item, idx) => {
+
+              console.log(item);
+              return(
+                <circle
+                cx = {campaignScale(item)}
+                cy = {25}
+                r = "3"
+                />
+              );
+            })}
+          </g>
+     
         </svg>
       </div>
     );
