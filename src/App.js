0cbd7e5554018ecb046f36324d32a1b92fa42bf3
Shuhei Koizumi <mozi3021@gmail.com>
2022-01-27 05:05:25 +0900

    ボタンを正しい位置に

--- src/App.js
+++ src/App.js
@@ -165,9 +165,7 @@ function App() {
   //let idx = 0;
   return (
     <div>
-      <div className="header">
-        <h1>Anime viz</h1>
-      </div>
+      
       <div className="hamburger">
         <input
           type="checkbox"
@@ -220,6 +218,9 @@ function App() {
             </div>
           </section>
         </nav>
+      </div>
+      <div className="header">
+        <h1>Anime viz</h1>
       </div>
       <div className="graph">
         <div className={classes.graphs}>
